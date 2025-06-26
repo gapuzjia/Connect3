0f2547ad61f16dd798642a80ea182ba458e6db98
Jia <jiannagapuz@gmail.com>
2025-06-26 16:26:07 -0700

    copied working program into main, modified makefile

--- Makefile
+++ Makefile
@@ -1,20 +1,2 @@
-# Name of the final executable
-TARGET = game
-
-# Source files
-SRC = main.c
-
-# Compiler and flags
-CC = gcc
-CFLAGS = -Wall -Wextra -std=c99
-
-# Default rule: build the executable
-all: $(TARGET)
-
-$(TARGET): $(SRC)
-	$(CC) $(CFLAGS) -o $(TARGET) $(SRC)
-
-# Clean up build artifacts
-clean:
-	rm -f $(TARGET)
-
+all:
+	gcc -o game main.c
--- main.c
+++ main.c
@@ -7,9 +7,8 @@ void printGameBoard(char gameBoard[9])
 	for(int i = 0; i < 9; i++)
 	{
 		printf("%c ", gameBoard[i]);
+		printf("%s", (i + 1) % 3 == 0 ? "\n" : "");
 
-		if((i + 1) % 3 == 0)
-		       printf("\n");	
 	}
 }
 
@@ -21,13 +20,10 @@ int checkIllegalMove(int position, char gameBoard[9])
 
 
 	//check if position is occupied
-	if(gameBoard[position] > 57) //using ASCII
-	{
-		printf("Spot is already occupied!\n");
-		return 1;
-	}
+	return (gameBoard[position] > 57) ? (printf("Spot is already occupied!\n"), 1) : 0;
 
-	return 0;
+
+	
 }
 
 void modifyGameBoard(char player, int position, char gameBoard[9])
@@ -43,17 +39,20 @@ int checkWinner(char player, char gameBoard[9])
 {
 	int a, b, c;
 	
-	int winningCombos[8][3] = {
-	      	{0,1,2}, {3,4,5}, {6,7,8},
-	       	{0,3,6}, {1,4,7}, {2,5,8},
-	       	{0,4,8}, {2,4,6}
+	int winningCombos[24] ={
+
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 3, 6, 1, 4, 7, 2, 5, 8,
+		6, 4, 2, 0, 4, 8
 	};
 	
-	for(int i = 0; i < 8; i++)
+	//winning combos are grouped into 3's
+	//skip count and check by groups of 3
+	for(int i = 0; i < 24; i+=3)
 	{
-		a = winningCombos[i][0];
-		b = winningCombos[i][1];
-		c = winningCombos[i][2];
+		a = winningCombos[i];
+		b = winningCombos[i + 1];
+		c = winningCombos[i + 2];
 
 		if(gameBoard[a] == gameBoard[b] && gameBoard[b] == gameBoard[c])
 		{
@@ -94,12 +93,7 @@ int main() {
 	{
 
 		//determine who's turn it is
-		
-		if(turns % 2 != 0)
-			player = 'X';
-		else
-			player = 'O';
-
+		(turns % 2 != 0) ? (player = 'X') : (player = 'O');
 
 		//print gameBoard
 		printGameBoard(gameBoard);
@@ -120,11 +114,8 @@ int main() {
 		isPlaying = !checkWinner(player, gameBoard);
 
 		//9 turns and no winner means a draw game
-		if(isPlaying && turns == 9)
-		{
-			printf("Draw Game!\n");
-			isPlaying = 0;		
-		}
+		(isPlaying && turns == 9) ? (printf("Draw Game!\n")), isPlaying = 0 : (void)0;
+
 		
 		//reset flags and update turn counter
 		turns++;
@@ -133,6 +124,18 @@ int main() {
 
 	printf("GAME OVER\n");
 	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
