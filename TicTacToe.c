3de06f930baf865647c0a4129f280ce2a284ff5b
Jia <jiannagapuz@gmail.com>
2025-06-27 11:32:02 -0700

    revised using structs, needs debugging

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,68 +1,81 @@
 #include <stdio.h>
 
-//-----MODIFIED-----adjusted to print 1d array instead
-void printGameBoard(char gameBoard[9])
-{
-	for(int i = 0; i < 9; i++)
-	{
-		printf("%c ", gameBoard[i]);
-		printf("%s", (i + 1) % 3 == 0 ? "\n" : "");
+//-----ADDED-----replace 1d array, implement with struct instead
+typedef struct {
+	char pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9;
+} GameBoard;
 
+
+
+//-----MODIFIED-----adjusted to print 1d array instead
+void printGameBoard(GameBoard gameBoard)
+{
+	printf("%c %c %c\n", gameBoard.pos1, gameBoard.pos2, gameBoard.pos3);
+	printf("%c %c %c\n", gameBoard.pos4, gameBoard.pos5, gameBoard.pos6);
+	printf("%c %c %c\n", gameBoard.pos7, gameBoard.pos8, gameBoard.pos9);
+
+}
+
+char* getPosition(GameBoard *gameBoard, int position)
+{
+	switch(position){
+		case 1: return &gameBoard->pos1;
+		case 2: return &gameBoard->pos2;
+		case 3: return &gameBoard->pos3;
+		case 4: return &gameBoard->pos4;
+		case 5: return &gameBoard->pos5;
+		case 6: return &gameBoard->pos6;
+		case 7: return &gameBoard->pos7;
+		case 8: return &gameBoard->pos8;
+		case 9: return &gameBoard->pos9;
+		default: return NULL;
 	}
+
 }
 
 
-int checkIllegalMove(int position, char gameBoard[9])
+int checkIllegalMove(int position, GameBoard *gameBoard)
 {	
-	//-----MODIFIED----- no longer need get position function
-	position = position - 1;
-
+	//-----MODIFIED-----revised getPosition function, now implemented on a struct
+	char *structPosition = getPosition(gameBoard, position); 
+	
 
 	//check if position is occupied
-	return (gameBoard[position] > 57) ? (printf("Spot is already occupied!\n"), 1) : 0;
-
-
+	//use ASCII value 57 to check if the char is not a digit (i.e in its place is an 'X' or 'O')
+	return (*structPosition > 57) ? (printf("Spot is already occupied!\n"), 1) : 0;
 	
 }
 
-void modifyGameBoard(char player, int position, char gameBoard[9])
+void modifyGameBoard(char player, int position, GameBoard *gameBoard)
 {
+	
+	//-----MODIFIED-----revised getPosition function, now implemented on a struct	
+	char *structPosition = getPosition(GameBoard, position); 
 
-	//-----MODIFIED----- no longer need get position function
-	position = position - 1;
-	gameBoard[position] = player;
+
+	*structPosition = player;
 }
 
 //-----MODIFIED----- replace if-else chain with reference 2d array
-int checkWinner(char player, char gameBoard[9])
+int checkWinner(char player, GameBoard gameBoard)
 {
-	int a, b, c;
-	
-	int winningCombos[24] ={
 
-		0, 1, 2, 3, 4, 5, 6, 7, 8,
-		0, 3, 6, 1, 4, 7, 2, 5, 8,
-		6, 4, 2, 0, 4, 8
-	};
-	
-	//winning combos are grouped into 3's
-	//skip count and check by groups of 3
-	for(int i = 0; i < 24; i+=3)
-	{
-		a = winningCombos[i];
-		b = winningCombos[i + 1];
-		c = winningCombos[i + 2];
+	if(
+		(gameBoard.pos1 == gameBoard.pos2 && gameBoard.pos2 == gameBoard.pos3) ||
+		(gameBoard.pos4 == gameBoard.pos5 && gameBoard.pos5 == gameBoard.pos6) ||
+		(gameBoard.pos7 == gameBoard.pos8 && gameBoard.pos8 == gameBoard.pos9) ||
+		(gameBoard.pos1 == gameBoard.pos4 && gameBoard.pos4 == gameBoard.pos7) ||
+		(gameBoard.pos2 == gameBoard.pos5 && gameBoard.pos5 == gameBoard.pos8) ||
+		(gameBoard.pos3 == gameBoard.pos6 && gameBoard.pos6 == gameBoard.pos9) ||
+		(gameBoard.pos1 == gameBoard.pos5 && gameBoard.pos5 == gameBoard.pos9) ||
+		(gameBoard.pos7 == gameBoard.pos5 && gameBoard.pos5 == gameBoard.pos3))
 
-		if(gameBoard[a] == gameBoard[b] && gameBoard[b] == gameBoard[c])
 		{
-			
-			printGameBoard(gameBoard);
 			printf("Player %c Wins!!!\n", player);
-		  	return 1;	
+			return 1;
 		}
-	}
 
-	//fall through means return 0 (no winner)
+	//code falls through, means no winner yet
 	return 0;
 }
 
@@ -80,9 +93,10 @@ int main() {
 	int turns = 1; 
 	
 	//-----MODIFIED----- scratch the 2d array, implement with 1d array instead
-	char gameBoard[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	//char gameBoard[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-
+	//create instance of a gameboard
+	GameBoard gameboard = {'1','2','3','4','5','6','7','8','9'};
 
 
 	printf("TicTacToe: \n");
