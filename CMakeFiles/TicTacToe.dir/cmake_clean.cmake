a6bc42a9e507206bb8c49fe9a1f09b867774cb36
Jia <jiannagapuz@gmail.com>
2025-06-29 11:08:35 -0700

    replaced Makefile with CMakeLists

---
+++ CMakeFiles/TicTacToe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToe.dir/link.d"
+  "CMakeFiles/TicTacToe.dir/TicTacToe.c.o"
+  "CMakeFiles/TicTacToe.dir/TicTacToe.c.o.d"
+  "TicTacToe"
+  "TicTacToe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TicTacToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
